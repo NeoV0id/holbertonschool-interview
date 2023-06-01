d378be78a6c43b5391a1dc174d0df78ef59c4df9
Neo <neochronos.chaos9@gmail.com>
2023-06-01 18:38:24 +0200

    Added files

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Given an n x n 2D matrix,
+rotate it 90 degrees clockwise. """
+
+def rotate_2d_matrix(matrix):
+
+    N = len(matrix)
+
+    matrix_copy = []
+    copy_row = 0
+    for column in range(N):
+        for row in range((N - 1), -1, -1):
+            if column == 0:
+                matrix_copy.append([])
+            matrix_copy[copy_row].append(matrix[row][column])
+        copy_row += 1
+    for row in range(N):
+        for column in range(N):
+            matrix[row][column] = matrix_copy[row][column]
---
+++ 0x16-rotate_2d_matrix/README.md
@@ -0,0 +1,2 @@
+This directory is for project 0x16-rotate_2d_matrix. It contains:
+- 0-rotate_2d_matrix.py
